5c9e729d38d379c77800e387ae4ce59fde5e09e8
Bell App Lab <apps@bellapplab.com>
2016-10-07 15:42:49 +0100

    DAMN YOU COCOAPODS

--- Example/Pods/Target Support Files/Orientation/Orientation-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Orientation : NSObject
-@end
-@implementation PodsDummy_Orientation
-@end
--- Example/Pods/Target Support Files/Orientation/Orientation-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double OrientationVersionNumber;
-FOUNDATION_EXPORT const unsigned char OrientationVersionString[];
-
